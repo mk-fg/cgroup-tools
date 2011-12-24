1a3e7224826e52f3933bd4ebf3514c29114275da
Mike Kazantsev <mk.fraggod@gmail.com>
2011-12-24 22:09:23 +0600

    cgput: update cgroup.procs as well

--- cgput.py
+++ cgput.py
@@ -4,19 +4,20 @@ from __future__ import print_function
 
 import argparse
 parser = argparse.ArgumentParser(
-	description='Put given pids into a control group.')
+	description='Put given tids/pids into a control group.')
 parser.add_argument('cgroup', help='Cgroup name.')
-parser.add_argument('pids', nargs='+', help='Process ids.')
+parser.add_argument('ids', nargs='+', help='Process/thread ids.')
 optz = parser.parse_args()
 
+import itertools as it, operator as op, functools as ft
 from glob import iglob
 import os, sys
 
-dst_set = set(map( os.path.realpath,
-	iglob('/sys/fs/cgroup/*/{}/tasks'.format(optz.cgroup)) ))
+dst_set = set(it.imap(os.path.realpath, it.chain(
+	iglob('/sys/fs/cgroup/*/{}/tasks'.format(optz.cgroup)),
+	iglob('/sys/fs/cgroup/*/{}/cgroup.procs'.format(optz.cgroup)) )))
 if not dst_set:
 	parser.error('Cgroup not found in any hierarchy: {}'.format(optz.cgroup))
 
-for pid in optz.pids:
-	for dst in dst_set:
-		open(dst, 'wb').write('{}\n'.format(pid))
+for dst, tid in it.product(dst_set, optz.ids):
+	open(dst, 'wb').write('{}\n'.format(tid))
