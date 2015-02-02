58192c8c859bc5977c41d8a454ed69cc78988959
Mike Kazantsev <mk.fraggod@gmail.com>
2015-02-02 22:07:56 +0500

    cgls: warn about pids missing in one of the rc's, but not others

--- cgls.py
+++ cgls.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals, print_function
+from __future__ import print_function
 
 import argparse
 parser = argparse.ArgumentParser(
 	description='List pids (tgids), belonging to a specified cgroups.')
 parser.add_argument('cgroups', nargs='+', help='Cgroup(s) to operate on.')
+parser.add_argument('--no-checks',
+	action='store_true', help='Do not perform sanity checks on pid sets.')
 argz = parser.parse_args()
 
 cg_root = '/sys/fs/cgroup'
@@ -15,24 +17,35 @@ from os.path import join, isdir
 from glob import glob
 import os, sys
 
-pids = set()
-
 listdirs = lambda path: it.ifilter( isdir,
 	it.imap(ft.partial(join, path), os.listdir(path)) )
-collect_pids = lambda path:\
-	pids.update(int(line.strip()) for line in open(join(path, 'cgroup.procs')))
+collect_pids = lambda path, pids_set:\
+	pids_set.update(int(line.strip()) for line in open(join(path, 'cgroup.procs')))
 
-def collect_pids_recurse(path):
-	collect_pids(path)
-	for sp in listdirs(path): collect_pids_recurse(sp)
+def collect_pids_recurse(path, pids_set):
+	collect_pids(path, pids_set)
+	for sp in listdirs(path): collect_pids_recurse(sp, pids_set)
+
+pids = dict()
 
 for rc in os.listdir(cg_root):
+	pids[rc] = pids_set = set()
 	for cg_name in argz.cgroups:
 		cg = 'tagged/{}'.format(cg_name)
 		cg_path = glob(join(cg_root, rc, cg))\
 			or glob(join(cg_root, rc, cg.replace('/', '.')))\
 			or glob('{}.*'.format(join(cg_root, rc, cg.replace('/', '.'))))
-		if not cg_path: continue # could be present in other rc's
-		for path in cg_path: collect_pids_recurse(path)
+		if not cg_path:
+			del pids[rc]
+			continue
+		for path in cg_path: collect_pids_recurse(path, pids_set)
 
-sys.stdout.write(''.join(it.imap('{}\n'.format, pids)))
+if not argz.no_checks:
+	for (ak, a), (bk, b) in it.combinations(pids.items(), 2):
+		pids_diff = a.symmetric_difference(b)
+		if not pids_diff: continue
+		print( 'Difference between rc pid sets {}/{}: {}'\
+			.format(ak, bk, ' '.join(map(bytes, pids_diff))), file=sys.stderr )
+
+pids_all = reduce(op.or_, pids.viewvalues(), set())
+sys.stdout.write(''.join(it.imap('{}\n'.format, pids_all)))
